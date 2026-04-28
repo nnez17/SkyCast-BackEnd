18c60007e1ab71f5bcf7928e2e4e3ecfc3ddb97a
nnez17 <novala1710@gmail.com>
2026-04-28 19:04:38 +0700

    dev: change cors origin

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { cors } from "@elysiajs/cors";
 const app = new Elysia()
   .use(
     cors({
-      origin: ["https://nnez17.github.io/"],
+      origin: ["https://nnez17.github.io/SkyCast/"],
     }),
   )
   .get("/weather", async ({ query }) => {
