2312689943850cfe6bdc66774b0c7dfd556f7e89
nnez17 <novala1710@gmail.com>
2026-04-28 10:02:06 +0700

    feat: Initial Commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,50 @@
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(
+    cors({
+      origin: ["https://nnez17.github.io/"],
+    }),
+  )
+  .get("/weather", async ({ query }) => {
+    const res = await fetch(
+      query.city
+        ? `https://api.openweathermap.org/data/2.5/weather?q=${query.city}&units=metric&appid=${process.env.API_KEY}`
+        : `https://api.openweathermap.org/data/2.5/weather?lat=${query.lat}&lon=${query.lon}&units=metric&appid=${process.env.API_KEY}`,
+    );
+    return res.json();
+  })
+  .get("/forecast", async ({ query }) => {
+    const res = await fetch(
+      query.city
+        ? `https://api.openweathermap.org/data/2.5/forecast?q=${query.city}&units=metric&appid=${process.env.API_KEY}`
+        : `https://api.openweathermap.org/data/2.5/forecast?lat=${query.lat}&lon=${query.lon}&units=metric&appid=${process.env.API_KEY}`,
+    );
+    return res.json();
+  })
+  .get("/air_pollution", async ({ query }) => {
+    const res = await fetch(
+      query.city
+        ? `https://api.openweathermap.org/data/2.5/air_pollution?q=${query.city}&units=metric&appid=${process.env.API_KEY}`
+        : `https://api.openweathermap.org/data/2.5/air_pollution?lat=${query.lat}&lon=${query.lon}&units=metric&appid=${process.env.API_KEY}`,
+    );
+    return res.json();
+  })
+  .get("/reverse_geo", async ({ query }) => {
+    const res = await fetch(
+      `https://api.openweathermap.org/geo/1.0/reverse?lat=${query.lat}&lon=${query.lon}&limit=5&appid=${process.env.API_KEY}`,
+    );
+    return res.json();
+  })
+  .get("/geo", async ({ query }) => {
+    const res = await fetch(
+      `https://api.openweathermap.org/geo/1.0/direct?q=${query.q}&limit=5&appid=${process.env.API_KEY}`,
+    );
+    return res.json();
+  })
+  .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
